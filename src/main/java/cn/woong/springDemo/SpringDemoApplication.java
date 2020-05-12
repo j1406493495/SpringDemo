483e8e92e819bcf35ec759d67ee4e7cc2cbed311
Woong <jin_wang_hui@163.com>
2020-05-12 20:24:45 +0800

    [Bug XXXX] hello Spring Cause: None Solution: None Fixed Version: None Branch: None

--- src/main/java/cn/woong/springDemo/SpringDemoApplication.java
+++ src/main/java/cn/woong/springDemo/SpringDemoApplication.java
@@ -2,12 +2,20 @@ package cn.woong.springDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 // 该注解开启组件扫描和自动配置
 @SpringBootApplication
+@RestController
 public class SpringDemoApplication {
 	public static void main(String[] args) {
 	    // 负责启动引导应用程序
 		SpringApplication.run(SpringDemoApplication.class, args);
+	}
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return "hello Spring";
 	}
 }
