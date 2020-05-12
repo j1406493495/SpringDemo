9955aaa0c0771565fc7a3a69b40c0a5ec8d0a5c4
Woong <jin_wang_hui@163.com>
2020-05-12 15:20:09 +0800

    [Bug XXXX] can use Cause: None Solution: None Fixed Version: None Branch: None

--- src/main/java/cn/woong/springDemo/SpringDemoApplication.java
+++ src/main/java/cn/woong/springDemo/SpringDemoApplication.java
@@ -6,10 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // 该注解开启组件扫描和自动配置
 @SpringBootApplication
 public class SpringDemoApplication {
-
 	public static void main(String[] args) {
 	    // 负责启动引导应用程序
 		SpringApplication.run(SpringDemoApplication.class, args);
 	}
-
 }
